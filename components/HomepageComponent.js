5d0fb703ba912a273db2df18830cd7bad92772c0
Jared Wright <jawerty210@gmail.com>
2016-05-16 18:08:08 -0400

    changes

--- components/HomepageComponent.js
+++ components/HomepageComponent.js
@@ -1,16 +1,20 @@
+import BigComponent from '../lib/big-component'
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import Image from './ImageComponent';
 import cats from '../data/cats';
 
-class HomepageComponent extends Component {
-
-  render() {
-    return (
+BigComponent()
+class HomepageComponent extends BigComponent {
+  view() {
+    const jsx = (
       <section>
         <h1>Hello World</h1>
         {cats.map(({image, description}) => <Image src={image} desc={description} />)}
       </section>
     );
+
+    return jsx
   }
 }
 
